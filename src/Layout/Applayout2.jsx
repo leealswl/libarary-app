5801542d52dbf5632c34ace45bbf22d1a9e2de6f
leealswl <alswl54321600@gmail.com>
2025-04-17 22:01:03 +0900

    first commit

--- src/Layout/Applayout2.jsx
+++ src/Layout/Applayout2.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Applayout2.style.css'
 
--- src/mainCard/Maincard.jsx
+++ src/mainCard/Maincard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const Maincard = () => {
